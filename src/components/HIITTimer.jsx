b54695b2c6602a13d5a4f2463e7a8835774dc2a3
EdgeFirewalk <thedinsorpaswer2@mail.ru>
2024-05-09 22:45:31 +0600

    Now the timer actually works, switches between states and plays sounds

--- src/components/HIITTimer.jsx
+++ src/components/HIITTimer.jsx
@@ -1,16 +1,82 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import useSound from 'use-sound';
 
 import '../styles/HIITTimer.scss';
 
 import SettingsIcon from '../img/settings-icon.svg';
 import PreviousExerciseIcon from '../img/previous-icon.svg';
 import PlayIcon from '../img/play-icon.svg';
+import PauseIcon from '../img/pause-icon.svg';
 import ResetIcon from '../img/reset-icon.svg';
 import NextExerciseIcon from '../img/next-icon.svg';
+
+import OpenSettingsSound from '../sounds/settings-open.mp3';
+import CloseSettingsSound from '../sounds/settings-close.mp3';
+import CountdownSound from '../sounds/countdown.mp3';
+import BeepSound from '../sounds/beep.mp3';
 
 import AppButton from './AppButton';
 
 const HIITTimer = () => {
+    /* Timer things */
+    const [timerSettings, setTimerSettings] = useState({
+        workTime: 45,
+        restTime: 15
+    });
+    const [currentTime, setCurrentTime] = useState(timerSettings.workTime);
+    const [isTimerRunning, setIsTimerRunning] = useState(false);
+    const [timerID, setTimerID] = useState(null);
+
+    /* Workout/Exercises things */
+    const [workoutState, setWorkoutState] = useState('work'); // 'work' or 'rest'
+
+    /* UI sounds */
+    const [playOpenSettingsSound] = useSound(OpenSettingsSound);
+    const [playCloseSettingsSound] = useSound(CloseSettingsSound);
+    const [playCountdownSound] = useSound(CountdownSound);
+    const [playBeepSound] = useSound(BeepSound);
+
+    function openTimerSettings() {
+        playOpenSettingsSound();
+    }
+
+    useEffect(() => {
+        if (currentTime <= 3 && currentTime !== 0) {
+            playCountdownSound();
+        }
+
+        if (currentTime <= 0) {
+            if (workoutState === 'work') {
+                setCurrentTime(timerSettings.restTime);
+                setWorkoutState('rest');
+            }
+            else {
+                setCurrentTime(timerSettings.workTime);
+                setWorkoutState('work');
+            }
+            playBeepSound();
+        }
+    }, [currentTime, workoutState, timerSettings.restTime, timerSettings.workTime]);
+
+    function startOrPauseTimer() {
+        if (!isTimerRunning) {
+            setTimerID(setInterval(() => {
+                setCurrentTime((prevTime) => prevTime - 1);
+            }, 1000));
+            setIsTimerRunning(true);
+        }
+        else {
+            clearInterval(timerID);
+            setIsTimerRunning(false);
+        }
+    }
+
+    function resetTimer() {
+        workoutState === 'work'
+            ? setCurrentTime(timerSettings.workTime)
+            : setCurrentTime(timerSettings.restTime);
+    }
+
     return (
         <div className="hiit-timer">
             <div className="container">
@@ -18,13 +84,20 @@ const HIITTimer = () => {
                     <AppButton
                         AdditionalClass="hiit-timer__settings"
                         ButtonIcon={SettingsIcon}
+                        OnClick={openTimerSettings}
                     />
-                    <div className="hiit-timer__stage">Work</div>
-                    <div className="hiit-timer__time">45</div>
+                    <div className="hiit-timer__stage">{workoutState}</div>
+                    <div className="hiit-timer__time">{currentTime}</div>
                     <div className="timer-buttons">
                         <AppButton ButtonIcon={PreviousExerciseIcon} />
-                        <AppButton ButtonIcon={PlayIcon} />
-                        <AppButton ButtonIcon={ResetIcon} />
+                        <AppButton
+                            ButtonIcon={isTimerRunning ? PauseIcon : PlayIcon}
+                            OnClick={startOrPauseTimer}
+                        />
+                        <AppButton
+                            ButtonIcon={ResetIcon}
+                            OnClick={resetTimer}
+                        />
                         <AppButton ButtonIcon={NextExerciseIcon} />
                     </div>
                 </div>
