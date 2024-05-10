c5c02706e5650668964438ce4d95ac7d9ae03ce7
EdgeFirewalk <thedinsorpaswer2@mail.ru>
2024-05-10 21:18:19 +0600

    Add timer settings modal to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
+import { useState } from 'react';
+
 import './styles/App.scss';
 
+import TimerSettings from './components/TimerSettings';
 import HIITTimer from './components/HIITTimer';
 
 function App() {
+    const [isTimerSettingsWindowOpen, setIsTimerSettingsWindowOpen] =
+        useState(false);
+
+    function openOrCloseTimerSettingsWindow() {
+        !isTimerSettingsWindowOpen
+            ? setIsTimerSettingsWindowOpen(true)
+            : setIsTimerSettingsWindowOpen(false);
+    }
+
     return (
         <div className="App">
-            <HIITTimer />
+            <TimerSettings
+                OpenOrCloseTimerSettingsWindow={openOrCloseTimerSettingsWindow}
+                TimerSettingsStyle={{
+                    visibility: isTimerSettingsWindowOpen ? 'visible' : 'hidden'
+                }}
+            />
+            <HIITTimer
+                OpenOrCloseTimerSettingsWindow={openOrCloseTimerSettingsWindow}
+            />
         </div>
     );
 }
