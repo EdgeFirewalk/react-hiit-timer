a3633036e420f7175cd8e8423c14228465444784
EdgeFirewalk <thedinsorpaswer2@mail.ru>
2024-06-07 00:06:15 +0600

    Remove hotkeys tips, prettify code

--- src/components/TimerTips.jsx
+++ src/components/TimerTips.jsx
@@ -6,25 +6,20 @@ import CloseIcon from '../img/close-icon.svg';
 
 import AppButton from './AppButton';
 
-const TimerTips = ({OpenOrCloseTimerTipsWindow, TimerTipsWindowStyle}) => {
+const TimerTips = ({ OpenOrCloseTimerTipsWindow, TimerTipsWindowStyle }) => {
     return (
         <div className="timer-tips modal-window" style={TimerTipsWindowStyle}>
-            <div className='container'>
-                <div className='modal-window__inner'>
+            <div className="container">
+                <div className="modal-window__inner">
                     <AppButton
                         ButtonIcon={CloseIcon}
                         AdditionalClass="modal-window__close-button"
                         OnClick={OpenOrCloseTimerTipsWindow}
                     />
-                    <p className="timer-tips__title App-title">Hotkeys</p>
-                    <p className='timer-tips__tip'>
-                        <span className='timer-tips__span'>[SPACE]</span> — start/pause timer at any time.
-                    </p>
-                    <p className='timer-tips__tip'>
-                        <span className='timer-tips__span'>[R]</span> — restart timer at any time.
-                    </p>
-                    <p className='timer-tips__tip'>
-                        <span className='timer-tips__span'>[ESC]</span> — close modal windows such as this one.
+                    <p className="timer-tips__title App-title">Tips</p>
+                    <p className="timer-tips__tip">
+                        <span className="timer-tips__span">Something</span> —
+                        there may be something important here someday...
                     </p>
                 </div>
             </div>
