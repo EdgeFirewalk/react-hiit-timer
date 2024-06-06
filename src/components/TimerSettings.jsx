62ec7cac77ac266391f628df90bfb89c6f4e3689
EdgeFirewalk <thedinsorpaswer2@mail.ru>
2024-06-07 00:05:36 +0600

    Create timer settings validation

--- src/components/TimerSettings.jsx
+++ src/components/TimerSettings.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 
 import '../styles/TimerSettings.scss';
 
@@ -12,31 +12,64 @@ const TimerSettings = ({
     OpenOrCloseTimerSettingsWindow,
     TimerSettingsWindowStyle
 }) => {
-    function setNewTimerSettings(settingName, newTimeValue, defaultTimeValue) {
-        const newTimerSettings = {
-            ...TimerSettings,
-            [settingName]:
-                !isNaN(parseInt(newTimeValue)) &&
-                parseInt(newTimeValue) <= 999 &&
-                parseInt(newTimeValue) !== 0
-                    ? parseInt(newTimeValue)
-                    : defaultTimeValue
-        };
+    const [hasErrorInWorkTimeSetting, setHasErrorInWorkTimeSetting] =
+        useState(false);
+    const [hasErrorInRestTimeSetting, setHasErrorInRestTimeSetting] =
+        useState(false);
 
-        SetTimerSettings(newTimerSettings);
-        localStorage.setItem(
-            'savedTimerSettings',
-            JSON.stringify(newTimerSettings)
-        );
+    const workTimeRef = useRef();
+    const restTimeRef = useRef();
+
+    function setNewTimerSettings(settingName, newTimeValue, inputRef) {
+        // Даже если неправильные настройки, то всё-равно присваиваем их состоянию
+        // и заодно показываем пользователю, что он ошибся
+        SetTimerSettings({ ...TimerSettings, [settingName]: newTimeValue });
+
+        const intNewTimeValue = parseInt(newTimeValue);
+        if (
+            isNaN(intNewTimeValue) ||
+            intNewTimeValue < 5 ||
+            intNewTimeValue > 999
+        ) {
+            inputRef.current.style.color = '#f61212';
+            inputRef.current.style.backgroundColor = 'rgba(246, 18, 18, 0.15)';
+            inputRef.current.style.borderBottomColor = '#f61212';
+
+            settingName === 'workTime'
+                ? setHasErrorInWorkTimeSetting(true)
+                : setHasErrorInRestTimeSetting(true);
+        } else {
+            inputRef.current.style.color = null;
+            inputRef.current.style.backgroundColor = null;
+            inputRef.current.style.borderBottomColor = null;
+
+            settingName === 'workTime'
+                ? setHasErrorInWorkTimeSetting(false)
+                : setHasErrorInRestTimeSetting(false);
+
+            const newTimerSettings = {
+                ...TimerSettings,
+                [settingName]: intNewTimeValue
+            };
+            SetTimerSettings(newTimerSettings);
+
+            localStorage.setItem(
+                'savedTimerSettings',
+                JSON.stringify(newTimerSettings)
+            );
+        }
     }
 
     return (
-        <div className="timer-settings modal-window" style={TimerSettingsWindowStyle}>
+        <div
+            className="timer-settings modal-window"
+            style={TimerSettingsWindowStyle}
+        >
             <div className="container">
                 <div className="modal-window__inner">
                     <AppButton
                         ButtonIcon={CloseIcon}
-                        AdditionalClass="modal-window__close-button"
+                        AdditionalClass={`modal-window__close-button ${hasErrorInWorkTimeSetting || hasErrorInRestTimeSetting ? 'modal-window__hidden-button' : ''}`}
                         OnClick={OpenOrCloseTimerSettingsWindow}
                     />
                     <p className="timer-settings__title App-title">Settings</p>
@@ -50,6 +83,7 @@ const TimerSettings = ({
                             </label>
                             <input
                                 id="work-input"
+                                ref={workTimeRef}
                                 className="settings-inputs__input"
                                 type="text"
                                 value={TimerSettings.workTime}
@@ -57,7 +91,7 @@ const TimerSettings = ({
                                     setNewTimerSettings(
                                         'workTime',
                                         e.target.value,
-                                        45
+                                        workTimeRef
                                     )
                                 }
                                 onClick={(e) => e.target.select()}
@@ -72,6 +106,7 @@ const TimerSettings = ({
                             </label>
                             <input
                                 id="rest-input"
+                                ref={restTimeRef}
                                 className="settings-inputs__input"
                                 type="text"
                                 value={TimerSettings.restTime}
@@ -79,16 +114,37 @@ const TimerSettings = ({
                                     setNewTimerSettings(
                                         'restTime',
                                         e.target.value,
-                                        15
+                                        restTimeRef
                                     )
                                 }
                                 onClick={(e) => e.target.select()}
                             />
                         </div>
                     </div>
-                    <p className="timer-settings__tip">
+                    <p
+                        className="timer-settings__tip"
+                        style={{
+                            display:
+                                hasErrorInWorkTimeSetting ||
+                                hasErrorInRestTimeSetting
+                                    ? 'none'
+                                    : 'block'
+                        }}
+                    >
                         New settings are saved and applied automatically, you
                         can just close this window!
+                    </p>
+                    <p
+                        className="timer-settings__error"
+                        style={{
+                            display:
+                                hasErrorInWorkTimeSetting ||
+                                hasErrorInRestTimeSetting
+                                    ? 'block'
+                                    : 'none'
+                        }}
+                    >
+                        Values must be numbers between 5 and 999.
                     </p>
                 </div>
             </div>
