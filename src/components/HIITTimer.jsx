7cdbd4f6f57eee838ecc160d8ec89060a0b552dd
EdgeFirewalk <thedinsorpaswer2@mail.ru>
2024-06-09 13:18:09 +0600

    Fix some bugs in the timer, hide unnecessary buttons and features until I get back to this project

--- src/components/HIITTimer.jsx
+++ src/components/HIITTimer.jsx
@@ -4,13 +4,13 @@ import useSound from 'use-sound';
 
 import '../styles/HIITTimer.scss';
 
-import QuestionIcon from '../img/question-icon.svg';
+//import QuestionIcon from '../img/question-icon.svg';
 import SettingsIcon from '../img/settings-icon.svg';
-import PreviousExerciseIcon from '../img/previous-icon.svg';
+//import PreviousExerciseIcon from '../img/previous-icon.svg';
 import PlayIcon from '../img/play-icon.svg';
 import PauseIcon from '../img/pause-icon.svg';
 import ResetIcon from '../img/reset-icon.svg';
-import NextExerciseIcon from '../img/next-icon.svg';
+//import NextExerciseIcon from '../img/next-icon.svg';
 
 import CountdownSound from '../sounds/countdown.mp3';
 import BeepSound from '../sounds/beep.mp3';
@@ -49,6 +49,11 @@ const HIITTimer = ({
         setIsTimerRunning((running) => !running);
     }
 
+    function resetTotalWorkTime() {
+        setTotalWorkTime(0);
+        setPrevStageTotalTime(0);
+    }
+
     function formatTotalWorkTime(seconds) {
         let hrs = Math.floor(seconds / 3600);
         let min = Math.floor((seconds % 3600) / 60);
@@ -60,7 +65,11 @@ const HIITTimer = ({
     }
 
     // Обновляем таймер, когда изменются настройки, чтобы они сразу применились
-    useEffect(() => resetTimer(), [TimerSettings]);
+    // Плюс сбрасываем общее время тренировки
+    useEffect(() => {
+        resetTimer();
+        resetTotalWorkTime();
+    }, [TimerSettings]);
 
     // Проигрываем звук, когда таймер останавливают или запускают
     useEffect(() => playCountdownSound(), [isTimerRunning]);
@@ -70,15 +79,24 @@ const HIITTimer = ({
             <div className="container">
                 <div className="hiit-timer__inner">
                     <div className="hiit-timer__header">
-                        <AppButton
+                        {/* This tips button will be needed when I add more functionality to this project */}
+                        {/*<AppButton
                             AdditionalClass="hiit-timer__tips"
                             ButtonIcon={QuestionIcon}
-                            OnClick={OpenOrCloseTimerTipsWindow}
-                        />
+                            OnClick={() => {
+                                if (isTimerRunning) { startOrPauseTimer(); }
+                                OpenOrCloseTimerTipsWindow();
+                            }}
+                        />*/}
                         <AppButton
                             AdditionalClass="hiit-timer__settings"
                             ButtonIcon={SettingsIcon}
-                            OnClick={OpenOrCloseTimerSettingsWindow}
+                            OnClick={() => {
+                                if (isTimerRunning) {
+                                    startOrPauseTimer();
+                                }
+                                OpenOrCloseTimerSettingsWindow();
+                            }}
                         />
                     </div>
                     <div className="hiit-timer__stage App-title">
@@ -99,8 +117,20 @@ const HIITTimer = ({
                             isPlaying={isTimerRunning}
                             duration={
                                 workoutState === 'work'
-                                    ? TimerSettings.workTime
-                                    : TimerSettings.restTime
+                                    ? // The settings input validation implemented is not gonna allow user to set time settings
+                                      // less than 5 (including negative values), but the used timer component does work with those numbers
+                                      // (including zero, but not the negative ones obviously)
+
+                                      // This kinda weird thing is done so that the used timer component doesn't throw a critical error for setting
+                                      // the wrong time to count down from (like 0 and 0 or negative values)
+                                      // These settings won't be saved actually and the user won't be able to close settings modal window until he
+                                      // sets the right time
+                                      TimerSettings.workTime > 0
+                                        ? TimerSettings.workTime
+                                        : 1
+                                    : TimerSettings.restTime > 0
+                                      ? TimerSettings.restTime
+                                      : 1
                             }
                             onUpdate={(remainingTime) => {
                                 setTotalWorkTime(
@@ -146,7 +176,8 @@ const HIITTimer = ({
                         </CountdownCircleTimer>
                     </div>
                     <div className="timer-buttons">
-                        <AppButton ButtonIcon={PreviousExerciseIcon} />
+                        {/* These buttons well be added if I get back to this project and actually add a feature with exercises */}
+                        {/*<AppButton ButtonIcon={PreviousExerciseIcon} />*/}
                         <AppButton
                             ButtonIcon={isTimerRunning ? PauseIcon : PlayIcon}
                             OnClick={startOrPauseTimer}
@@ -155,7 +186,7 @@ const HIITTimer = ({
                             ButtonIcon={ResetIcon}
                             OnClick={resetTimer}
                         />
-                        <AppButton ButtonIcon={NextExerciseIcon} />
+                        {/*<AppButton ButtonIcon={NextExerciseIcon} />*/}
                     </div>
                     <div className="hiit-timer__info">
                         <div className="info-block">
@@ -164,10 +195,10 @@ const HIITTimer = ({
                                 {formatTotalWorkTime(totalWorkTime)}
                             </div>
                         </div>
-                        <div className="info-block">
+                        {/*<div className="info-block">
                             <div className="info-block__title">Round №</div>
                             <div className="info-block__info">0</div>
-                        </div>
+                        </div>*/}
                     </div>
                 </div>
             </div>
